cf350637628c3cfff673f66103dad6e089170bf7
marga <miriamcristianamcg@gmail.com>
2018-04-30 23:33:05 +0100

    working endpoints and ids in the movies of the cinemas

--- server/app.js
+++ server/app.js
@@ -1,173 +1,187 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-const assert = require('assert');
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const assert = require("assert");
 const https = require("https");
-const config = require('./config/config');
+const config = require("./config/config");
 
 // Database Name
-const url = 'mongodb://localhost:27017';
+const url = "mongodb://localhost:27017";
 const dbName = config.dbName;
 
-app.get('/init', function (req, res) {
-    const cinemaData = require('./databaseinit.js');
-    res.send('Created database!');
+app.get("/init", async (req, res) => {
+  try {
+    const cinemaData = await require("./databaseinit.js");
+    return res.send("Created database!");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/', function (req, res) {
-    res.send();
+app.get("/", (req, res) => {
+  return res.send();
 });
 
-app.get('/movies', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('movies').find({}).project({ 'name': 1, 'imageurl': 1, 'genre': 1, 'duration': 1, 'minAge': 1 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            res.send(docs)
-        });
-    });
+app.get("/movies", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const docs = await db
+      .collection("movies")
+      .find({})
+      .project({ name: 1, imageurl: 1, genre: 1, duration: 1, minAge: 1 })
+      .toArray();
+    return res.json(docs);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-
-app.get('/movie/:name', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('movies').createIndex({ name: "text" })
-        db.collection('movies').find({ $text: { $search: req.params.name } }).project({ '_id': 0 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            if(docs.length == 1)
-                res.send(docs[0])
-            else
-                res.send(docs);
-        });
-    });
+app.get("/movies/:name", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    await db.collection("movies").createIndex({ name: "text" });
+    const docs = await db
+      .collection("movies")
+      .find({ $text: { $search: req.params.name } })
+      .project({ _id: 0 })
+      .toArray();
+    return res.json(docs);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
+app.get("/movieID/:id", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    let id = req.params.id;
 
+    if (id.indexOf('"') >= 0) {
+      id = JSON.parse(id);
+    }
 
-
-app.get('/movieID/:id', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        let id = req.params.id;
-        if (id.indexOf('\"') >= 0) {
-            id = JSON.parse(id)
-        }
-        db.collection('movies').findOne({ _id: new ObjectId(id)}, function (err, info) {
-            if (err)
-                throw err;
-            res.send(info)
-        });
-    });
+    const info = await db.collection("movies").findOne({ _id: new ObjectId(id) });
+    return res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/debuts', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('debuts').find({}).project({ 'name': 1, 'imageurl': 1, 'genre': 1, 'duration': 1, 'minAge': 1 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            res.send(docs)
-        });
-    });
+app.get("/debuts", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const docs = await db
+      .collection("debuts")
+      .find({})
+      .project({
+        name: 1,
+        imageurl: 1,
+        genre: 1,
+        duration: 1,
+        minAge: 1
+      })
+      .toArray();
+    return res.json(docs);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/debutID/:id', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        let id = req.params.id;
-        if (id.indexOf('\"') >= 0) {
-            id = JSON.parse(id)
-        }
-        db.collection('debuts').findOne({ _id: new ObjectId(id)}, function (err, info) {
-            if (err)
-                throw err;
-            res.send(info)
-        });
-    });
+app.get("/debutID/:id", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    let id = req.params.id;
+    if (id.indexOf('"') >= 0) {
+      id = JSON.parse(id);
+    }
+    const info = await db.collection("debuts").findOne({ _id: new ObjectId(id) });
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/debut/:name', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('debuts').createIndex({ name: "text" })
-        db.collection('debuts').find({ $text: { $search: req.params.name } }).project({ '_id': 0 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            if (docs.length == 1)
-                res.send(docs[0])
-            else
-                res.send(docs);
-        });
-    });
+app.get("/debut/:name", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const info = await db
+      .collection("debuts")
+      .find({ $text: { $search: req.params.name } })
+      .project({ _id: 0 })
+      .toArray();
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-
-app.get('/listCinemas', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('cinemas').find({}).project({'name': 1, 'address': 1 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            res.send(docs)
-        });
-    });
+app.get("/listCinemas", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const info = await db
+      .collection("cinemas")
+      .find({})
+      .project({ name: 1, address: 1 })
+      .toArray();
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/cinemaID/:id', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        let id = req.params.id;
-        if (id.indexOf('\"') >= 0) {
-            id = JSON.parse(id)
-        }
-        db.collection('cinemas').findOne({ _id: new ObjectId(id)}, function (err, info) {
-            if (err)
-                throw err;
-            res.send(info)
-        });
-    });
+app.get("/cinemaID/:id", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    let id = req.params.id;
+    if (id.indexOf('"') >= 0) {
+      id = JSON.parse(id);
+    }
+    const info = await db.collection("cinemas").findOne({ _id: new ObjectId(id) });
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/cinema/:name', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('cinemas').createIndex({ name: "text" })
-        db.collection('cinemas').find({ $text: { $search:  req.params.name } }).project({ '_id': 0, 'movies': 1 }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            if (docs.length == 1)
-                res.send(docs[0])
-            else
-                res.send(docs);
-        });
-    });
+app.get("/cinema/:name", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const info = await db
+      .collection("cinemas")
+      .find({ $text: { $search: req.params.name } })
+      .project({ _id: 0, movies: 1 })
+      .toArray();
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-app.get('/localizations', function (req, res) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) { return console.dir(err); }
-        const db = client.db(dbName);
-        db.collection('cinemas').find({}).project({ movies: false, address: false, telephone: false }).toArray(function (err, docs) {
-            if (err)
-                throw err;
-            res.send(docs)
-        });
-    });
+app.get("/localizations", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const info = await db
+      .collection("cinemas")
+      .find({})
+      .project({ movies: false, address: false, telephone: false })
+      .toArray();
+    res.json(info);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!');
+  console.log("Listening on port 3000!");
 });
